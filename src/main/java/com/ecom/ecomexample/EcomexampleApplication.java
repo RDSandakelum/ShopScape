cace5e0e6d641edcdf6d3f8ae90b1ae9948615b0
Dinushka Ramanayaka <dinushka.ramanayake@xyicon.com>
2024-09-18 21:13:16 +0530

    Initial commit

---
+++ src/main/java/com/ecom/ecomexample/EcomexampleApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.ecomexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomexampleApplication.class, args);
+	}
+
+}
